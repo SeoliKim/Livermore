ff0d8b7af00dc7bbeab2d61a0222a587e38f92ce
SeoliKim <108114194+SeoliKim@users.noreply.github.com>
2023-10-19 18:43:55 -0400

    Update main.py
    Mean Reversion 0.0

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
 # main branch empty file holder
+# region imports
+from AlgorithmImports import *
+# endregion
+
+class MeanReversionAlgorithm(QCAlgorithm):
+    
+    def Initialize(self) -> None:
+        # Basic Setup
+        self.SetStartDate(2017, 1, 1)
+        self.SetEndDate(2023, 1, 1)
+        self.SetCash(1000000)
+
+        # Parameter
+        self.period = 30 # hr
+        self.resolution= Resolution.Daily
+        self.lookback_period = 20
+        self.buy_threshold = -1.0  # Z-score threshold to buy
+        self.sell_threshold = 1.0  # Z-score threshold to sell
+
+        # dictionaries
+        self.smas={}
+        self.stds= {}
+
+        # stocks
+        self.tickers= [
+            "PG",
+            "JNJ",
+            "KO",
+            "PEP",
+            "WMT",
+            "HD",
+            "MCD",            
+            "CAT",
+            "V",
+            "MA"
+        ]
+        for t in self.tickers:
+            self.AddEquity(t, self.resolution)
+            self.smas[t] = self.SMA(t, self.period, self.resolution) 
+            self.stds[t]= self.STD(t,self.period)
+            
+        
+        self.Debug(self.smas)
+        
+    def OnData(self, slice):
+        if not slice.HasData:
+            return
+        
+        for ticker in self.tickers:
+            bar = slice.Bars.get(ticker)
+            if bar:
+                self.smas[ticker].Update(bar.EndTime, bar.Close)
+                self.stds[ticker].Update(bar.EndTime, bar.Close)
+
+                if self.smas[ticker].IsReady and self.stds[ticker].IsReady:
+                    # Calculate the mean and standard deviation
+                    close_price = bar.Close
+                    mean = self.smas[ticker].Current.Value
+                    std= self.stds[ticker].Current.Value
+
+                    # Calculate the Z-score
+                    z_score = (close_price - mean) / std
+
+                    if z_score < self.buy_threshold:
+                        # Buy signal: Z-score is below the buy threshold
+                        self.SetHoldings(ticker, 1.0)
+                    elif z_score > self.sell_threshold:
+                        # Sell signal: Z-score is above the sell threshold
+                        if self.Portfolio[ticker].Invested:
+                            # If ticker is in the portfolio, sell it
+                            self.SetHoldings(ticker, 0)
+                    
